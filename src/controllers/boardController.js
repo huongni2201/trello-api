f45c8847c36ef163c227d0512fa982acbbd5c2f9
Huong Nguyen <huongnn2201@gmail.com>
2025-01-09 23:37:40 +0700

    Create slug

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -5,11 +5,14 @@
  */
 
 import { StatusCodes } from 'http-status-codes'
+import { boardService } from '~/services/boardService'
 
 const createNew = async (req, res, next) => {
 
   try {
-    res.status(StatusCodes.CREATED).json({ message: 'POST from Controller: API create new broard' })
+    const createdBoard = await boardService.createNew(req.body)
+
+    res.status(StatusCodes.CREATED).json(createdBoard)
   } catch (error) { next(error) }
 }
 
